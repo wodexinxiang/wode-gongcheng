f1fcd38a687a39b212fa35d5ef821d010c5df1d9
wodexinxiang <45392241+wodexinxiang@users.noreply.github.com>
2018-11-27 20:55:16 +0800

    Add files via upload

---
+++ 001.js
@@ -0,0 +1,9 @@
+function getLength(r){
+	return 2*3.14*r;
+}
+
+ function getArea(r){
+ 	return 3.14*Math.pow(r,2);                                                                                                                                                                                                         )
+}
+module.exports.getLength=getLength;
+module.exports.getArea=getArea;
---
+++ 002.js
@@ -0,0 +1,3 @@
+var obj=require('./001')
+console.log(obj.gentLength(5))
+console.log(obj.getArea(5))
